77750e4fc8f826e2449a6a4b0739f95b54210e6b
bmindaugas <37580537+bmindaugas@users.noreply.github.com>
2018-07-07 14:42:02 +0300

    elektra su paskaita

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,118 @@
 package com.company;
-
+//import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int skSuma = 0;
+        int masIlgis = (int)(Math.random()*10)+1;
+        int[] masyvas100sk = new int[masIlgis];
+
+        System.out.println("Masyvo ilgis");
+
+//        int[]a = new int[10];
+//        System.out.println("masyvo ilgis "+a.length);
+//
+//        double r = Math.random()*10;
+//        int sas = (int) r +1;
+//
+//        System.out.println(a[i]);
+//        System.out.println(sas);
+
+
+
+//        double r = Math.random()*100;
+//        int sas = (int) r +1;
+//
+//        //kopijuoja masyva
+//        int[]m={123};
+//        int[]tm = new int[m.length+1];
+//        for(int i=0; i<m.length; i++){
+//            tm[i]=m[i];
+//        }
+
+//        int[][]dm=new int[2][3];
+//
+//        int[] m = new int[3];
+//        m[1]=5;
+//        System.out.println(m[1]);
+
+//        for (int i=0; i<m.length; i++){
+//            System.out.println(m[i]);
+//        }
+
+//        for (int i=1; i<=5; i++){
+//           if (i%2 != 0){
+//               for (int j = 1; j<=5; j++){
+//                   System.out.print(i*j+" ");
+//               }
+//           }
+//           else {
+//               for (int j=5; j>=1; j--){
+//                   System.out.print(i*j+" ");
+//               }
+//           }
+//            System.out.println();
+//            }
+
+//        int i = 1;
+//        while (i <= 10){
+//            int j = 1;
+//            while (j <= 10){
+//                System.out.print(i + "x");
+//            System.out.print(j + "=");
+//            System.out.println(i * j);
+//            j++;
+//        }
+//        i++;
+//    }
+//        for (int i=1; i<=10; i++){
+//            for (int j=1; j<=10; j++){
+//                System.out.print(i+"x");
+//                System.out.print(j+"=");
+//                System.out.println(i*j);
+//            }
+//        }
+
+
+//        for (int i=1; i<10; i+=3){
+//            System.out.println(il);
+//        }
+
+//        int i = 0;
+//        while ( i++<10 ) {
+//            System.out.println(i);
+//        }
+//        do {
+//            System.out.println(i);
+//        } while (++i<=10);
+//        System.out.println(i);
+
+
+//        Scanner klavetura = new Scanner(System.in);
+//
+//        double dienosKwh;
+//        double naktiesKwh;
+//        double bendrasKwh;
+//        double dienosEur = 0.13;
+//        double naktiesEur = 0.08;
+//        double abonementas = 3;
+//        double sumaDienos;
+//        double sumaNakties;
+//        double sumaBendra;
+//        System.out.println("Iveskite dienos Kwh: ");
+//        dienosKwh = klavetura.nextDouble();
+//        System.out.println("Iveskite nakties Kwh: ");
+//        naktiesKwh = klavetura.nextDouble();
+//
+//        bendrasKwh = dienosKwh + naktiesKwh;
+//        System.out.println("Dienos ir nakties sunaudota "+bendrasKwh+" Kwh.");
+//        sumaDienos = dienosKwh * dienosEur;
+//        sumaNakties = naktiesKwh * naktiesEur;
+//        sumaBendra = sumaDienos + sumaNakties + abonementas;
+//        System.out.println("Sunaudota diena "+dienosKwh+" Kwh "+sumaDienos+" Eur. ");
+//        System.out.println("Sunaudota nakti "+naktiesKwh+ " Kwh "+sumaNakties+" Eur. ");
+//        System.out.println("Suma yra "+sumaBendra+" Eur.");
+
     }
 }
